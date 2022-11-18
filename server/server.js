93d02dd88e9e4653384fee98df22f0e4f4fcc7a4
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-11-18 18:46:57 +0530

    wikipedia api count updated into  20

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 app.get('/',async (req, res) => {
 	let { search } = req?.query;
 	const response =await  axios.get(
-		`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=2&srsearch=${search}`
+		`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`
 	);
 	res.send(response?.data?.query?.search);
 });
